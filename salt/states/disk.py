9008699013afe3e99cc765108e69eb70a397c9e7
remy.d1 <remy.d1@gmail.com>
2016-03-11 17:12:02 +0100

    enable blkid checking for disks

--- salt/states/disk.py
+++ salt/states/disk.py
@@ -3,6 +3,22 @@
 Disk monitoring state
 
 Monitor the state of disk resources
+
+Check blkid and test disk type
+
+.. code-block:: yaml
+  
+  disk_status:
+    disk.status:
+      - name: /media/sda1
+      - maximum: 95%
+      - minimum: 5%
+
+  disk_blkid:
+    disk.blkid:
+        - name: /dev/sda1
+        - disktype: zfs
+
 '''
 from __future__ import absolute_import
 
@@ -12,6 +28,37 @@ from salt.ext.six import string_types
 __monitor__ = [
         'status',
         ]
+
+def blkid(name, disktype):
+    '''
+    Return the blkid informations like disk.blkid
+    No changes will be made
+
+    '''
+    ret = {'name': name,
+           'result': False,
+           'comment': '',
+           'raw_value' : '',
+           'type': '',
+           'changes': {}}
+
+    data = __salt__['disk.blkid']()
+    if name not in data:
+        ret['result'] = False
+        ret['comment'] = 'Named disk not present: {0}'.format(name)
+        return ret
+    else:
+        ret['result'] = True
+        if disktype:
+            if disktype in data[name]['TYPE']:
+                ret['type'] = data[name]['TYPE']
+                data[name]['comment'] = 'Your {0} type has been found !'.format(disktype)
+                ret['comment'] = data[name]
+            else:
+                ret['result'] = False
+                ret['comment'] = 'Your {0} type has not been found !'.format(disktype)
+        return ret
+
 
 
 def status(name, maximum=None, minimum=None):
@@ -51,12 +98,12 @@ def status(name, maximum=None, minimum=None):
     ret['data'] = data[name]
     if minimum:
         if cap < minimum:
-            ret['comment'] = 'Disk is below minimum of {0} at {1}'.format(
+            ret['comment'] = 'Disk is below minimum of {0}% at {1}'.format(
                     minimum, cap)
             return ret
     if maximum:
         if cap > maximum:
-            ret['comment'] = 'Disk is above maximum of {0} at {1}'.format(
+            ret['comment'] = 'Disk is above maximum of {0}% at {1}'.format(
                     maximum, cap)
             return ret
     ret['comment'] = 'Disk in acceptable range'
